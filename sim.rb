96e659d5c4b3650dffcace25c186825391aa9ae7
Justin Buchanan <justbuchanan@gmail.com>
2013-12-26 17:31:13 -0500

    sim.rb now uses the new Game object in greed.rb

--- sim.rb
+++ sim.rb
@@ -21,5 +21,56 @@ include Greed
 
 
 
-probs = (1..6).map { |dice_count| probability_of_not_busting(dice_count) }
-puts "Probabilities: #{probs}"
+# probs = (1..6).map { |dice_count| probability_of_not_busting(dice_count) }
+# puts "Probabilities: #{probs}"
+
+
+# this player keeps rolling until they get 500+ for the round
+# also they always choose the first option
+player = lambda do |player_id, scores, choices, round_running_total|
+	choice = choices[0]
+	return choice, !(round_running_total + choice[1] >= 500)
+end
+
+
+
+game = Game.new([
+	player,
+	player,
+	player
+])
+
+
+game.run()
+
+
+
+# dice_count = 6
+# score = 0
+# while true
+# 	dice = roll(dice_count)
+# 	puts "Rolled: #{dice}"
+# 	choices = choices(dice)
+# 	if choices.length == 0
+# 		puts "Bust! :("
+# 		break
+# 	end
+
+# 	choice = choices.sort do |a, b|
+# 		b[1] <=> a[1]
+# 	end.first
+# 	dice_count -= choice[0].length
+# 	score += choice[1]
+# 	puts "chose #{choice[0]} for a score of #{choice[1]}, total is now #{score}"
+
+# 	if dice_count == 0
+# 		dice_count = 6
+# 		puts "get all the dice again!"
+# 	end
+
+# 	puts ""
+# end
+
+
+
+
