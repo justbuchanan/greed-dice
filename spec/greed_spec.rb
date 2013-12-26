334d45681fe1904f045991355a96d58ef6fa84b0
Justin Buchanan <justbuchanan@gmail.com>
2013-12-26 11:26:47 -0500

    updated greed_spec and added a new test

--- spec/greed_spec.rb
+++ spec/greed_spec.rb
@@ -12,14 +12,20 @@ describe Greed do
 	context 'choices' do
 		let(:straight) { (1..6).to_a }
 		let(:three_pair) { [3, 3, 4, 4, 6, 6] }
+		let(:example_roll) { [5, 4, 3, 1, 2, 1] }
 
 
 		it 'recognizes a straight' do
-			expect(choices(straight)).should include([straight, STRAIGHT_VALUE])
+			expect(choices(straight)).to include([straight, STRAIGHT_VALUE])
 		end
 
 		it 'recognizes three-pair' do
-			expect(choices(three_pair)).should include([three_pair, THREE_PAIR_VALUE])
+			expect(choices(three_pair)).to include([three_pair, THREE_PAIR_VALUE])
+		end
+
+		it 'allows splitting ones' do
+			expect(choices(example_roll)).to include([[1], 100])
+			expect(choices(example_roll)).to include([[1, 1], 200])
 		end
 	end
 end
