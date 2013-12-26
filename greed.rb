ce0263c290e690ba301da1fe07547825372b14b3
Justin Buchanan <justbuchanan@gmail.com>
2013-12-26 00:37:36 -0500

    working on choice generation

--- greed.rb
+++ greed.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 
 module Greed
 	STRAIGHT_VALUE = 1000
@@ -17,6 +19,12 @@ module Greed
 			counts[val] = c if c > 0
 			counts
 		end
+	end
+
+
+	def evaluate_dice(dice)
+		#FIXME: implement
+		return -1
 	end
 
 
@@ -45,21 +53,62 @@ module Greed
 
 
 		# discard anything that's not a 1 or 5 or has a count less than 3
-		counts = counts.map do |die, count|
-			if die != 5 && die != 1 && count < 3
-				nil
-			else
-				{ die => count }
+		counts = counts.inject({}) do |filtered, pair|
+			die = pair.first
+			count = pair[1]
+			if die == 5 || die == 1 || count >= 3
+				filtered[die] = count
 			end
+			filtered
 		end
 
 
+		options_by_index = counts.inject([]) do |opts, pair|
+			die = pair.first
+			count = pair[1]
+			if die == 5 || die == 1
+				opts << (0..count).to_a
+			else
+				opts << [0] + (3..count).to_a
+			end
+			opts
+		end
 
-		#### each choice is an array of numbers ()
-		# [1, 1, 1] => 1000
-		# [a, a, a] => a * 100
-		# 1 => 100
-		# 5 => 50
+		choices_by_index = [0] * counts.size
+
+		# advances to the next combination
+		# returns false if it was unable to get a new combination
+		tumble = lambda do
+			for index in (choices_by_index.length-1..0)
+				if choices_by_index[index] < options_by_index[index].length - 1
+					++choices_by_index[index]
+
+					# reset all choices after the one just tumbled
+					for index in (index+1..choices_by_index.length)
+						choices_by_index[index] = 0;
+					end
+					return true
+				end
+			end
+
+			return false
+		end
+
+		# builds the choice from
+		extract = lambda do
+			choice = []
+			(0..choices_by_index.length).each do |i|
+				choice += [ counts.keys[i] ] * options_by_index[i][ choices_by_index[i] ]
+			end
+			[choice, evaluate_dice(choice)]
+		end
+
+		# add the choice to the set of possible
+		while tumble.call()
+			p "tumble!"
+			choices << extract.call()
+		end
+
 
 		choices
 	end
